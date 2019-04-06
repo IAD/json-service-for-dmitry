647dfb42e9ee80a8c91b63384dc441862813c83a
Igor Dolgov <iadolgov@gmail.com>
2019-04-06 14:34:16 +0700

    🍋

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type doc struct {
+	CPU       float64  `json:"cpu"`
+	Hostnames []string `json:"hostname"`
+}
+
+type results map[string]doc
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	idle0, total0 := getCPUSample()
+	time.Sleep(1 * time.Second)
+	idle1, total1 := getCPUSample()
+
+	idleTicks := float64(idle1 - idle0)
+	totalTicks := float64(total1 - total0)
+	cpuUsage := 100 * (totalTicks - idleTicks) / totalTicks
+
+	result := make(results, 0)
+	result["pc-1"] = doc{
+		CPU:       cpuUsage,
+		Hostnames: []string{"1", "2"},
+	}
+	result["pc-2"] = doc{
+		CPU:       cpuUsage,
+		Hostnames: []string{"3", "4"},
+	}
+
+	body, _ := json.MarshalIndent(result, "", "   ")
+
+	fmt.Fprintf(w, "%s", string(body))
+}
+
+func getCPUSample() (idle, total uint64) {
+	contents, err := ioutil.ReadFile("/proc/stat")
+	if err != nil {
+		return
+	}
+	lines := strings.Split(string(contents), "\n")
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if fields[0] == "cpu" {
+			numFields := len(fields)
+			for i := 1; i < numFields; i++ {
+				val, err := strconv.ParseUint(fields[i], 10, 64)
+				if err != nil {
+					fmt.Println("Error: ", i, fields[i], err)
+				}
+				total += val // tally up all the numbers to get total ticks
+				if i == 4 {  // idle is the 5th field in the cpu line
+					idle = val
+				}
+			}
+			return
+		}
+	}
+	return
+}
